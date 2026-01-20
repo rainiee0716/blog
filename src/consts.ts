edacbfe4d0d15198427f430035b9eab0fa06987f
vectorzhang <295253797@qq.com>
2026-01-21 00:18:13 +0800

    init astro blog

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Rainiee Blog';
+export const SITE_DESCRIPTION = '记录 AI 学习与职场成长的真实思考!';
